13adb0cf191194af09766a6965ffe876aa14367b
Todd B. Stein <toddbstein@gmail.com>
2015-08-31 21:31:07 -0700

    many improvements including lockfile, failed download protection, private range protection, and performance optimizations.

--- adblocker.sh
+++ adblocker.sh
@@ -17,45 +17,85 @@ HOST_LISTS="
 BLOCKLIST=/tmp/adblocker_hostlist
 BLACKLIST=/etc/adblocker_blacklist
 WHITELIST=/etc/adblocker_whitelist
+LOCKFILE=/tmp/adblocker.lock
 
-# get script's absolute path and escape spaces
+
+# ensure this is the only instance running
+if ! ln -s $$ "$LOCKFILE" 2>/dev/null; then
+	# if the old instance is still running, exit
+	former_pid=$(readlink "$LOCKFILE")
+	if [ -e "/proc/$former_pid" ]; then
+		exit
+	else
+		# otherwise, update the symlink
+		ln -sf $$ "$LOCKFILE"
+	fi
+fi
+
+
+# get script's absolute path and quote spaces for safety
 cd "${0%/*}"
 SCRIPT_NAME="$PWD/${0##*/}"
 SCRIPT_NAME="${SCRIPT_NAME// /' '}"
 cd "$OLDPWD"
+
 
 # await internet connectivity before proceeding (in case rc.local executes this script before connectivity is achieved)
 until ping -c1 -w3 google.com || ping -c1 -w3 yahoo.com; do
 	sleep 5
 done &>/dev/null
 
-# initialize block list
->"$BLOCKLIST"
 
-# grab blacklisted domains if any have been specified
-[ -s "$BLACKLIST" ] && awk '/^[^#]/ { print "0.0.0.0",$1 }' "$BLACKLIST" >>"$BLOCKLIST"
-
-# grab host lists from the internet
+# grab list of bad domains from the internet
 IP_REGEX='([0-9]{1,3}\.){3}[0-9]{1,3}'
-wget -qO- $HOST_LISTS | sed -rn "s/^$IP_REGEX\W/0.0.0.0 /p" | awk '{ print $1,$2 }' | sort -uk2 >>"$BLOCKLIST"
+hosts=$(wget -qO- $HOST_LISTS | awk "/^$IP_REGEX\W/"'{ print "0.0.0.0",$2 }' | sort -uk2)
+
+
+# if the download succeeded, recreate the blocklist
+if [ -n "$hosts" ]; then
+	# add downloaded domains to a fresh block list
+	printf "%s\n" "$hosts" >"$BLOCKLIST"
+fi
+
+
+# add blacklisted domains if any have been specified, ensuring no duplicates are added
+if [ -s "$BLACKLIST" ]; then
+	# create a pipe-delimited list of all non-commented words in blacklist and remove them from the block list
+	black_listed_regex='\W('"$(grep -o '^[^#]\+' "$BLACKLIST" | xargs | tr ' ' '|')"')$'
+	sed -ri "/${black_listed_regex//./\.}/d" "$BLOCKLIST"
+
+	# add blacklisted domains to block list	
+	awk '/^[^#]/ { print "0.0.0.0",$1 }' "$BLACKLIST" >>"$BLOCKLIST"
+fi
+
+
+# remove any private net IP addresses (just in case)
+# this variable contains a regex which will be used to prevent the blocking of hosts on 192.168.0.0 and 10.0.0.0 networks
+PROTECTED_RANGES='\W(192\.168(\.[0-9]{1,3}){2}|10(\.[0-9]{1,3}){3})$'
+sed -ri "/$PROTECTED_RANGES/d" "$BLOCKLIST"
+
 
 # remove any whitelisted domains from the block list
 if [ -s "$WHITELIST" ]; then
-	# create a pipe-delimited list of all non-commented words in whitelist
-	white_listed_regex=`echo \`grep -o '^[^#]\+' "$WHITELIST"\` | tr ' ' '|'`
-	sed -ri "/$white_listed_regex/d" "$BLOCKLIST"
+	# create a pipe-delimited list of all non-commented words in whitelist and remove them from the block list
+	white_listed_regex='\W('"$(grep -Eo '^[^#]+' "$WHITELIST" | xargs | tr ' ' '|')"')$'
+	sed -ri "/${white_listed_regex//./\.}/d" "$BLOCKLIST"
 fi
+
 
 # add IPv6 blocking
 sed -ri 's/([^ ]+)$/\1\n::      \1/' "$BLOCKLIST"
+
 
 # add block list to dnsmasq config if it's not already there
 if ! uci get dhcp.@dnsmasq[0].addnhosts | grep -q "$BLOCKLIST"; then
 	uci add_list dhcp.@dnsmasq[0].addnhosts="$BLOCKLIST" && uci commit
 fi
 
+
 # restart dnsmasq service
 /etc/init.d/dnsmasq restart
+
 
 # carefully add script to /etc/rc.local if it's not already there
 if ! grep -Fq "$SCRIPT_NAME" /etc/rc.local; then
@@ -77,11 +117,18 @@ if ! grep -Fq "$SCRIPT_NAME" /etc/rc.local; then
 	rm -f /tmp/rc.local.new
 fi
 
+
 # add script to root's crontab if it's not already there
-# adds 30 minutes of jitter to prevent undue load on the webservers hosting the lists we pull each week
-# unfortunately, there's no $RANDOM in this shell, so:
-DELAY='$(head /dev/urandom | wc -c | /usr/bin/awk "{ print \$0 % 30 }")m'
-grep -Fq "$SCRIPT_NAME" /etc/crontabs/root 2>/dev/null || cat >>/etc/crontabs/root <<-:EOF:
-	# Download updated ad and malware server lists every Tuesday between 3:00 and 3:30 AM
-	0 3 * * 2 /bin/sleep $DELAY && $SCRIPT_NAME
-:EOF:
+if ! grep -Fq "$SCRIPT_NAME" /etc/crontabs/root 2>/dev/null; then
+	# adds 30 minutes of jitter to prevent undue load on the webservers hosting the lists we pull each week
+	# unfortunately, there's no $RANDOM in this shell, so:
+	DELAY=$(head /dev/urandom | wc -c | /usr/bin/awk "{ print \$0 % 30 }")
+	cat >>/etc/crontabs/root <<-:EOF:
+		# Download updated ad and malware server lists every Tuesday at 3:$(printf "%02d" "$DELAY") AM
+		$DELAY 3 * * 2 $SCRIPT_NAME
+	:EOF:
+fi
+
+
+# clean up
+rm -f "$LOCKFILE"
