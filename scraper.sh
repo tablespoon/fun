cb95caa7d816be28ba4c1d97b44bbfa6ed95e053
Todd Stein <toddbstein@gmail.com>
2021-11-22 15:02:26 -0800

    added verification retries to mitigate false positives

--- scraper.sh
+++ scraper.sh
@@ -3,30 +3,25 @@
 set -x
 
 MAX_DELAY=3     # maximum delay (in minutes) between queries
+VERIFICATION_RETRIES=3	# number of times to retest a site to try to prevent false positives
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0"
 
 
-# email-to-sms addresses
 recipients=(
-	"@vtext.com"
-	"@txt.att.net"
+	"123456789@vtext.com"
+	"123456789@txt.att.net"
+	"example@gmail.com"
 )
-
-# urls to scrape
 sites=(
 	"https://www.amazon.com/PlayStation-5-Digital/dp/B08FC6MR62/"
 	"https://www.target.com/c/playstation-5-video-games/-/N-hj96d?lnk=snav_rd_playstation_5"
 	"https://www.gamestop.com/consoles-hardware/playstation-5/consoles?prefn1=buryMaster&prefv1=In%20Stock&view=new"
 )
-
-# corresponding site names
 names=(
 	"Amazon"
 	"Target"
 	"GameStop"
 )
-
-# corresponding "product not found" strings
 strings=(
 	"Currently unavailable"
 	"Consoles will be viewable when inventory is available."
@@ -43,15 +38,23 @@ while true; do
 
 		[[ ${notification_sent[$i]} ]] && continue
 
-		curl -s --compressed -A "$USER_AGENT" "${sites[$i]}" | grep -q "${strings[$i]}"
-		if [[ $? -ne 0 ]]; then
-			notification_sent[$i]="yes"
-			for recipient in ${recipients[@]}; do
-				msmtp "$recipient" <<<"Playstation 5 may be in stock at ${names[$i]}!
+		# if we can't find the string after VERIFICATION_RETRIES, send alert
+		retry_counter=0
+		while ! curl -s --compressed -A "$USER_AGENT" "${sites[$i]}" | grep -q "${strings[$i]}"; do
+
+			if [[ $retry_counter -eq $VERIFICATION_RETRIES ]]; then
+				notification_sent[$i]="yes"
+				for recipient in ${recipients[@]}; do
+					msmtp "$recipient" <<<"Playstation 5 may be in stock at ${names[$i]}!
 
 ${sites[$i]}"
-			done
-		fi
+				done
+				break
+			fi
+			((retry_counter++))
+				
+		done
+
 	done
 
 	# we're done when alerts have been sent for all tracked sites
