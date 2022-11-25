af390bedea96716406754a4cd3d6bd5983ad27f4
Todd Stein <toddbstein@gmail.com>
2022-11-25 13:30:12 -0800

    initial checkin - lists possible wordle solutions given exposed clues. This can be used to cheat. Don't do that.

---
+++ wordle/wordle_options.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+file=words
+
+# parse and process arguments
+for clue in ${@}; do
+	color=${clue:0:1}
+	position=${clue:1:1}
+	letter=${clue:2}
+
+	case $color in
+		g) green[$position]=$letter;;
+		y) yellow[$position]+=$letter;;
+		x) eliminated+=${clue:1};;
+	esac
+
+done
+
+# compile regex from what we know about each position
+for ((i=1; i<=$(wc -L <$file); i++)); do
+	if [[ ${green[$i]} ]]; then
+		word[$i]=${green[$i]}
+	elif [[ ${yellow[$i]} ]]; then
+		word[$i]=[^${yellow[$i]}]
+		yellows+=${yellow[$i]}
+	else
+		word[$i]=.
+	fi
+done
+word=${word[@]} 
+word=${word// /}
+
+# build initial wordlist - remove words with eliminated letters and grab words that match regex from remaining set
+words=$(grep -v "[$eliminated]" $file | grep -E "^$word$")
+
+# get unique list of yellows
+yellows=$(sed 's/./&\n/g' <<<$yellows | sort -u)
+
+# remove words that don't contain all yellows
+for i in $yellows; do
+	words=$(grep ${i} <<<"$words")
+done
+
+echo "$words"
